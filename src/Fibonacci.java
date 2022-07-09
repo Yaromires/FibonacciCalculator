af5ded3ae50ba9c5677b63146a0a67e6723d48d5
Yaromires <savyaromir@gmail.com>
2022-07-09 16:55:45 +0300

    Update

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,10 +2,6 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-        printFibonacciNumbers();
-    }
-
-    private static void printFibonacciNumbers() {
         for (int f : calculateFibonacciNumbers()) {
             System.out.println(f);
         }
@@ -13,6 +9,7 @@ public class Fibonacci {
 
     private static int[] calculateFibonacciNumbers() {
         int[] fibonacci = new int[47];
+        fibonacci[0] = 0;
         fibonacci[1] = 1;
         for (int i = 2; i < fibonacci.length; i++) {
             fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
